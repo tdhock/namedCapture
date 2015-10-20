faf909cbc54c210f611c42844a7927f8ea422366
Toby Dylan Hocking <tdhock5@gmail.com>
2015-10-20 15:35:19 -0400

    explain

--- tests/testthat/test-NA.R
+++ tests/testthat/test-NA.R
@@ -26,6 +26,11 @@ subject.vec <- c("this will match", rep(NA, N))
 pattern <- paste0("(?<name>ill)(?<rest>.*)")
 
 test_that("NA subjects do not cause warnings", {
+  ## Sometimes the C code underly regexpr will return large ints in
+  ## capture start/length for subjects with missing values, and in
+  ## that case we were seeing the warning "NAs introduced by coercion
+  ## to integer range." but sometimes (randomly) it did not cause a
+  ## warning.
   expect_no_warning({
     computed.mat <- str_match_named(subject.vec, pattern)
   })
