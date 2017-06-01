56c5df6c50fbf22082fe91ae8ea7bc3efb08717e
Toby Dylan Hocking <tdhock5@gmail.com>
2017-06-01 09:33:56 -0400

    examples

--- R/str_match.R
+++ R/str_match.R
@@ -1,4 +1,4 @@
-str_match_named <- function
+str_match_named <- structure(function
 ### Parse the first occurance of pattern from each of several subject
 ### strings using a named capture regular expression.
 (subject.vec,
@@ -30,9 +30,30 @@ str_match_named <- function
 ### matrix. Otherwise if there is a group named "name" then it will
 ### not be returned as a column, and will instead be used for the
 ### rownames.
-}
+}, ex=function(){
+  library(namedCapture)
+  chr.pos.vec <- c(
+    "chr10:213,054,000-213,055,000",
+    "chrM:111,000-222,000",
+    "this will not match",
+    NA, # neither will this.
+    "chr1:110-111 chr2:220-222") # two possible matches.
+  chr.pos.pattern <- paste0(
+    "(?<chrom>chr.*?)",
+    ":",
+    "(?<chromStart>.*?)",
+    "-",
+    "(?<chromEnd>[0-9,]*)")
+  ## Specifying a list of conversion functions means that str_match_*
+  ## should convert the matched groups from character to whatever is
+  ## returned by those functions.
+  keep.digits <- function(x)as.integer(gsub("[^0-9]", "", x))
+  conversion.list <- list(chromStart=keep.digits, chromEnd=keep.digits)
+  (match.df <- str_match_named(chr.pos.vec, chr.pos.pattern, conversion.list))
+  str(match.df)
+})
 
-str_match_all_named <- function
+str_match_all_named <- structure(function
 ### Parse several occurances of pattern from each of several subject
 ### strings using named capturing regular expressions.
 (subject.vec,
@@ -73,7 +94,42 @@ str_match_all_named <- function
 ### column, and will instead be used for the rownames of the
 ### data.frames or matrices. If subject.vec has names, they will be
 ### used as the names of the returned list.
-}
+}, ex=function(){
+  library(namedCapture)
+  chr.pos.vec <- c(
+    "chr10:213,054,000-213,055,000",
+    "chrM:111,000-222,000",
+    "this will not match",
+    NA, # neither will this.
+    "chr1:110-111 chr2:220-222") # two possible matches.
+  chr.pos.pattern <- paste0(
+    "(?<chrom>chr.*?)",
+    ":",
+    "(?<chromStart>.*?)",
+    "-",
+    "(?<chromEnd>[0-9,]*)")
+  ## Specifying a list of conversion functions means that str_match_*
+  ## should convert the matched groups from character to whatever is
+  ## returned by those functions.
+  keep.digits <- function(x)as.integer(gsub("[^0-9]", "", x))
+  conversion.list <- list(chromStart=keep.digits, chromEnd=keep.digits)
+  ## Use str_match_all_named to get ALL matches in each subject (not
+  ## just the first match).
+  (match.df.list <- str_match_all_named(
+    chr.pos.vec, chr.pos.pattern, conversion.list))
+  str(match.df.list)
+  ## If there is a capture group named "name" then it will be used for
+  ## the rownames of the result.
+  name.value.vec <- c(
+    H3K27me3="  sampleType=monocyte   assayType=H3K27me3    cost=5",
+    H3K27ac="sampleType=monocyte assayType=H3K27ac",
+    H3K4me3=" sampleType=Myeloidcell cost=30.5  assayType=H3K4me3")
+  name.value.pattern <- paste0(
+    "(?<name>[^ ]+?)",
+    "=",
+    "(?<value>[^ ]+)")
+  str_match_all_named(name.value.vec, name.value.pattern)
+})
 
 apply_type_funs <- function
 ### Convert columns of match.mat using corresponding functions from
