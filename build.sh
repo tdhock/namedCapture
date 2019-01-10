d7883b47d9832fe540f54d6e6908cfac44077500
Toby Hocking <tdhock5@gmail.com>
2019-01-09 20:13:51 -0800

    readme comparison table

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 rm -rf namedCapture-release
 cp -r namedCapture namedCapture-release
 PKG_TGZ=$(R CMD build namedCapture-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
-R CMD INSTALL $PKG_TGZ
-R CMD check --as-cran $PKG_TGZ
+R --vanilla CMD INSTALL $PKG_TGZ
+R --vanilla CMD check --as-cran $PKG_TGZ
