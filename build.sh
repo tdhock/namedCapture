7f0fbe6ade820ae0b6e42347ed68d267ad4005c4
Toby Dylan Hocking <tdhock5@gmail.com>
2019-03-07 11:48:04 -0700

    new vignettes

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cd ..
 set -o errexit
 rm -rf namedCapture-release
 cp -r namedCapture namedCapture-release
-PKG_TGZ=$(R CMD build namedCapture-release|grep building|sed "s/.*\(namedCapture.*.tar.gz\).*/\1/")
+PKG_TGZ=$(R CMD build namedCapture-release|tee namedCapture-release-build.txt|grep building|sed "s/.*\(namedCapture.*.tar.gz\).*/\1/")
+cat namedCapture-release-build.txt
 R --vanilla CMD INSTALL $PKG_TGZ
 R --vanilla CMD check --as-cran $PKG_TGZ
